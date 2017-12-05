56ef0452644f6c44d9ef45d1f56b0428baa2a82d
Nikhil Sharma (Mac) <n89sharma@gmail.com>
2017-12-04 22:53:07 -0500

    json for you know who

--- src/main/java/main/Database.java
+++ src/main/java/main/Database.java
@@ -1,11 +1,13 @@
 package main;
 
 import dbobjects.*;
-
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import static dbobjects.Tables.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Database {
 
@@ -24,6 +26,20 @@ public class Database {
         List<YieldAmount> yieldAmounts              = getAllTableRowsFrom(YIELD_AMOUNT);
         List<YieldName> yieldNames                  = getAllTableRowsFrom(YIELD_NAME);
         //formatter:on
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File("./ConversionFactors.json"), conversionFactors);
+        mapper.writeValue(new File("./FoodGroups.json"), foodGroups);
+        mapper.writeValue(new File("./foodNames.json"), foodNames);
+        mapper.writeValue(new File("./FoodSources.json"), foodSources);
+        mapper.writeValue(new File("./MeasureNames.json"), measureNames);
+        mapper.writeValue(new File("./NutrientAmounts.json"), nutrientAmounts);
+        mapper.writeValue(new File("./NutrientNames.json"), nutrientNames);
+        mapper.writeValue(new File("./NutrientSources.json"), nutrientSources);
+        mapper.writeValue(new File("./RefuseAmounts.json"), refuseAmounts);
+        mapper.writeValue(new File("./RefuseNames.json"), refuseNames);
+        mapper.writeValue(new File("./YieldAmounts.json"), yieldAmounts);
+        mapper.writeValue(new File("./YieldNames.json"), yieldNames);
     }
 
 }
