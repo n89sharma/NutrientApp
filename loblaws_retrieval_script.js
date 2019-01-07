f248483481758d02d660d7c1eaa0ebf3c570c969
Nikhil Sharma (Mac) <n89sharma@gmail.com>
2019-01-06 22:17:47 -0500

    cereal script updated

--- loblaws_retrieval_script.js
+++ loblaws_retrieval_script.js
@@ -1,8 +1,36 @@
 const axios = require('axios')
 const fs = require('fs');
 
-function get_items(page, elements_per_page, aisle_id, aisle_title) {
-    return axios.get('https://www.instacart.ca/v3/retailers/311/module_data/aisle_11642_61501', {
+
+function get_all_cereal_data(file_path) {
+
+    let get_all_items_url = 'https://www.instacart.ca/v3/retailers/311/module_data/aisle_11642_61501'
+    let get_item_url = 'https://www.instacart.ca/v3/containers/items/'
+    let cookie = 'remember_user_token=W1s0NTI2MDY2Nl0sIiQyYSQxMCRSaERrNHl1aEhHUGxLb09KdVpxU00uIiwiMTU0NjczMjQyMi44MzgyMjU0Il0%3D--9e90ab073ad8d18dbe91ba0221ef6837ffc0275f'
+    let pages = [1, 2, 3]
+    let elements_per_page = 100
+    let aisle_id = 61501
+    let aisle_title = 'Cereal'
+    let promises = []
+
+    pages.forEach(page => {
+        promises.push(get_items(get_all_items_url, get_item_url, page, elements_per_page, aisle_id, aisle_title, cookie));
+    });
+    axios.all(promises)
+        .then(results => {
+            let all_data = [].concat.apply([], results);
+            fs.writeFile(file_path, JSON.stringify(all_data), function (err) {
+                if (err) {
+                    console.log('writing error', err);
+                }
+            });
+        })
+        .catch(() => console.log('result object'));
+};
+
+function get_items(get_items_url, get_item_url, page, elements_per_page, aisle_id, aisle_title, cookie) {
+
+    return axios.get(get_items_url, {
         params: {
             aisle_title: aisle_title,
             aisle_id: aisle_id,
@@ -10,103 +38,97 @@ function get_items(page, elements_per_page, aisle_id, aisle_title) {
             per: elements_per_page
         },
         headers: {
-            Cookie: "remember_user_token=W1s0NTI2MDY2Nl0sIiQyYSQxMCRSaERrNHl1aEhHUGxLb09KdVpxU00uIiwiMTU0NjczMjQyMi44MzgyMjU0Il0%3D--9e90ab073ad8d18dbe91ba0221ef6837ffc0275f"
+            Cookie: cookie
         }
     })
         .then(response => {
-            console.log('here', page);
-            promises = []
-            nutrient_data = []
-            response.data.module_data.items.forEach(element => {
-                promises.push(
-                    get_nutrients(element.id).then(response => {
-                        nutrient_data.push({
-                            id: element.id,
-                            name: element.name,
-                            size: element.size,
-                            serving_size: response.serving_size,
-                            calories: response.calories,
-                            nutrients: response.nutrients
-                        });
-                    }))
+            let promises = [];
+            let nutrient_data = [];
+            response.data.module_data.items.forEach(item => {
+                setTimeout(function(){}, 35);
+                console.log(item.id);
+                promises.push(get_nutrients(get_item_url, item, cookie));
             });
-            return Promise.all(promises).then(() => {
-                return nutrient_data;
-            })
-        })
-        .catch(error => {
-            return new Promise(function (resolve, reject) {
-                resolve({ 'error': 'call failed' });
-            });
-        });
-}
-
-function get_nutrients(elementId) {
-    return axios.get('https://www.instacart.ca/v3/containers/items/' + elementId, {
-        headers: {
-            Cookie: "remember_user_token=W1s0NTI2MDY2Nl0sIiQyYSQxMCRSaERrNHl1aEhHUGxLb09KdVpxU00uIiwiMTU0NjczMjQyMi44MzgyMjU0Il0%3D--9e90ab073ad8d18dbe91ba0221ef6837ffc0275f"
-        }
-    })
-        .then(response => {
-            module = response.data.container.modules.find(element => {
-                return element.id == "item_details_attributes_66a1c26c89401b9e69f6e2c224c1ce1f"
-            });
-            if (module) {
-                nutrition = module.data.nutrition
-                nutrients = nutrition.nutrients
-                return new Promise(function (resolve, reject) {
-                    resolve({
-                        serving_size: nutrition.serving_size,
-                        calories: nutrition.calories,
-                        nutrients: nutrients
-                    })
-                });
-            } else {
-                return new Promise(function (resolve, reject) {
-                    resolve({ 'error': 'no module found' })
+            return axios.all(promises)
+                .then(results => {
+                    results.forEach(result => nutrient_data.push(result));
+                    return nutrient_data;
                 })
-            }
+                .catch(() => {
+                    return {
+                        error: 'error'
+                    }
+                });
         })
-        .catch(error => {
-            return new Promise(function (resolve, reject) {
-                resolve({ 'error': 'call 2 failed' })
-            })
-        })
+        .catch(error => console.log(JSON.stringify(error)));
 }
 
-function get_all_cereal_data() {
-    all_data = []
-    pages = [1,2,3]
-    elements_per_page = 100
-    aisle_id = 61501
-    aisle_title = 'Cereal'
-
-    promises = []
-    pages.forEach(page => {
-
-        promises.push(
-            get_items(page, elements_per_page, aisle_id, aisle_title)
-                .then(response => {
-                    all_data.push(response)
-                }))
+function get_nutrients(get_item_url, element, cookie) {
+    return axios.get(get_item_url + element.id, {
+        headers: {
+            Cookie: cookie
+        }
     })
-    Promise.all(promises).then(() => {
-        fs.writeFile('cereal.json', JSON.stringify(all_data), function (err) {
-            if (err) {
-                console.log(err);
+        .then(response => {
+            let modules = response.data.container.modules
+            let module = modules.find(module => {
+                return module.id === "item_details_attributes_66a1c26c89401b9e69f6e2c224c1ce1f"
+            });
+            if (!!module) {
+                let nutrition = module.data.nutrition
+                return {
+                    id: element.id,
+                    name: element.name,
+                    size: element.size,
+                    serving_size: nutrition == null ? null : nutrition.serving_size ,
+                    calories: nutrition == null ? null : nutrition.calories,
+                    nutrients: nutrition == null ? null : nutrition.nutrients
+                };
+            } else {
+                return {
+                    error: {
+                        id: element.id,
+                        message: 'error - module not found',
+                        module: module,
+                        modules: modules
+                    }
+                };
             }
+        })
+        .catch(() => {
+            return {
+                error: {
+                    id: element.id,
+                    message: 'error - in nutrient retrieval'
+                }
+            };
         });
-    })
-};
+}
 
-function read_file() {
-    fs.readFile('cereal.json', 'utf8', (err, data) => {
+function read_file(file_path) {
+    fs.readFile(file_path, 'utf8', (err, data) => {
         data = JSON.parse(data)
-        console.log(data[0].length)
-        console.log(data[1].length)
-        console.log(data[2].length)
+        console.log(data.length)
+        blanks = data.filter(element => {
+            return !element.nutrients
+        })
+        blanks.forEach(element => console.log(element.id))
+        console.log(blanks.length)
     })
 }
 
-// get_all_cereal_data();
-read_file();
+
+let get_item_url = 'https://www.instacart.ca/v3/containers/items/'
+let cookie = 'remember_user_token=W1s0NTI2MDY2Nl0sIiQyYSQxMCRSaERrNHl1aEhHUGxLb09KdVpxU00uIiwiMTU0NjczMjQyMi44MzgyMjU0Il0%3D--9e90ab073ad8d18dbe91ba0221ef6837ffc0275f'
+file_path = 'cereal3.json'
+get_all_cereal_data(file_path);
+// read_file(file_path);
+
+let item = {
+    id: 'item_465430072',
+    name: '',
+    size: ''
+}
+// get_nutrients(get_item_url, item, cookie).then(response => {
+//     console.log(response);
+// })
